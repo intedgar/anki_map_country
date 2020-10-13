2bb2911ac08f6ccce7255c17a0c8e439642876a6
int-edgar <e.schuette1@gmail.com>
2020-10-13 21:34:56 +0200

    update main.py and download_image.py to download images in higher pixel resolution

---
+++ download_image.py
@@ -0,0 +1,112 @@
+import requests
+#import beautiful soup 4
+import bs4
+import re
+import shutil
+
+def parse_page(url):
+    response = requests.get(url)
+    #print(response)
+    text = response.text
+    soup = bs4.BeautifulSoup(text, "lxml")
+    return soup
+
+
+def get_map_page_anchor(soup, continent):
+    anchor = soup.find(title=re.compile('in its region.svg'))
+
+    if anchor == None:
+        anchor = soup.find(title=re.compile(f'in its region [(]undisputed[)].svg'))
+    if anchor == None:
+        anchor = soup.find(title=re.compile(f'in {continent} [(]-rivers -mini map[)].svg'))
+    if anchor == None:
+        anchor = soup.find(title=re.compile(f'in {continent} [(]-mini map -rivers[)].svg'))
+    if anchor == None:
+        anchor = soup.find(title=re.compile(f'in {continent} [(]undisputed only[)] [(]-mini map -rivers[)].svg'))
+    if anchor == None:
+        anchor = soup.find(title=re.compile(f'in {continent} [(]undisputed[)] [(]-mini map -rivers[)].svg'))
+    if anchor == None:
+        anchor = soup.find(title=re.compile(f'in {continent} [(]only undisputed[)] [(]-mini map -rivers[)].svg'))
+    if anchor == None:
+        anchor = soup.find(title=re.compile(f'in its region [(]de-facto[)].svg'))
+    if anchor == None:
+        anchor = soup.find(title=re.compile(f'in {continent} [(]de-facto[)] [(]-mini map -rivers[)].svg'))
+
+    return anchor
+
+
+def get_map_image(soup, continent):
+    image = soup.find(alt=re.compile(f'in its region.svg'))
+    if image == None:
+        image = soup.find(alt=re.compile(f'in its region [(]undisputed[)].svg'))
+    if image == None:
+        image = soup.find(alt=re.compile(f'in {continent} [(]-rivers -mini map[)].svg'))
+    if image == None:
+        image = soup.find(alt=re.compile(f'in {continent} [(]-mini map -rivers[)].svg'))
+    if image == None:
+        image = soup.find(alt=re.compile(f'in {continent} [(]undisputed only[)] [(]-mini map -rivers[)].svg'))
+    if image == None:
+        image = soup.find(alt=re.compile(f'in {continent} [(]undisputed[)] [(]-mini map -rivers[)].svg'))
+    if image == None:
+        image = soup.find(alt=re.compile(f'in {continent} [(]only undisputed[)] [(]-mini map -rivers[)].svg'))
+    if image == None:
+        image = soup.find(alt=re.compile(f'in its region [(]de-facto[)].svg'))
+    if image == None:
+        image = soup.find(alt=re.compile(f'in {continent} [(]de-facto[)] [(]-mini map -rivers[)].svg'))
+
+
+
+    print('continent', continent)
+    print('image', image)
+    return image
+
+
+def get_alternative_url(soup):
+    anchor = soup.find('a', attrs={'href':re.compile('/wiki/Category:SVG_locator_maps_of')})
+    print('anchor: ', anchor)
+    url = 'https://commons.wikimedia.org' + anchor['href']
+    print('url: ', url)
+    return url
+
+def get_map_page_url(anchor):
+    map_page_url = 'https://commons.wikimedia.org' + anchor['href']
+    return map_page_url
+
+
+def download_and_save_image(image_url, img_name):
+    response = requests.get(f"{image_url}", stream=True)
+
+    file = open(f'C:\\Users\\Eddie\\AppData\\Roaming\\Anki2\\Benutzer 1\\collection.media\\{img_name}.png', 'wb')
+    #file = open(f'C:\\Users\\Eddie\\Desktop\\Test\\{img_name}.png', 'wb')
+
+    file.write(response.content)
+
+    file.close()
+
+
+
+#test the functions
+if __name__ == '__main__':
+
+    html_soup = parse_page("https://commons.wikimedia.org/wiki/Category:SVG_locator_maps_of_Egypt_(location_map_scheme)?uselang=de")
+
+    anchor = get_map_page_anchor(html_soup, 'Africa')
+    print(anchor)
+
+    url = get_map_page_url(anchor)
+
+    html_soup = parse_page(url)
+    #print(html_soup.prettify())
+
+    anchor_tags = html_soup.find_all('a')
+    print(anchor_tags)
+    anchor = None
+    for tag in anchor_tags:
+        anchor_string = tag.string
+        if anchor_string == "Original file":
+            anchor = tag
+
+    #print(anchor)
+    image_url = anchor["href"]
+
+    download_and_save_image(image_url, "map_Ägypten")
---
+++ main.py
@@ -0,0 +1,103 @@
+import requests
+from bs4 import BeautifulSoup
+import download_image
+import re
+
+#send a request to wikipedia website with a list of all states
+response = requests.get('https://commons.wikimedia.org/wiki/Category:SVG_locator_maps_of_Albania_(location_map_scheme)?uselang=de')
+text = response.text
+soup = BeautifulSoup(text, "lxml")
+#print(soup.prettify())
+
+#select the div saying "Weitere" from the table
+div = soup.find('div', attrs={"aria-labelledby": re.compile("^Weitere")})
+#print(div)
+#select the table that is a child of the div
+table = div.find('table')
+#print('table: ', table)
+tbody = table.find('tbody')
+table_rows = tbody.find_all('tr')
+#print(table_rows)
+
+#create a new file to which will be written
+file = open('import.txt', 'w', encoding = 'utf-8')
+
+#countries left out:
+left_out = []
+
+#exclude table_rows not containing countries, so that the continents stay
+continent_rows = table_rows[1:7]
+#continent_rows = [continent_rows[2]]
+#print(continent_rows)
+for continent in continent_rows:
+    #navigate to extract continent name in English
+    th = continent.find('th')
+    #print('th', th)
+    b = th.find('b')
+    continent_name_anchor = b.find('a')
+    continent_name_anchor_title = continent_name_anchor["title"]
+
+    #pattern to find the names of the conteinents in English
+    pattern = re.compile('SVG locator maps of ([a-zA-Z]+[\s]*[a-zA-Z]*) [(]location')
+    matches = pattern.findall(continent_name_anchor_title)
+    continent_name = matches[0]
+
+    #navigate to extract all the countries in the table row
+    countries_div = continent.find('div')
+    anchors = countries_div.find_all('a', href=True)
+    #print(len(anchors))
+
+    #for loop over all the country anchors in each continent
+    for anchor in anchors:
+        #print('anchor:', anchor)
+        href = anchor["href"]
+        country = anchor.string
+        if country.startswith('(') and country.endswith(')'):
+            left_out.append({'continent': continent_name, 'country': country})
+            #print('left_out: ', left_out)
+            continue
+        #print('country: ', country)
+        url = "https://commons.wikimedia.org/" + href
+
+        #download and save image from the website
+        html_soup = download_image.parse_page(url)
+        anchor_tag = download_image.get_map_page_anchor(html_soup, continent_name)
+        #print('url', url)
+        
+        if anchor_tag == None:
+            parse_url = download_image.get_alternative_url(html_soup)
+            #print('parse_url: ', parse_url)
+            html_soup = download_image.parse_page(parse_url)
+            image = download_image.get_map_page_anchor(html_soup, continent_name)
+            #print('url', url)
+
+        if anchor_tag == None:
+            left_out.append({'continent': continent_name, 'country': country})
+            print('left_out: ', left_out)
+            continue
+
+        url = download_image.get_map_page_url(anchor_tag)
+        print('url', url)
+        html_soup = download_image.parse_page(url)
+
+        div = html_soup.find('div', class_="mw-filepage-resolutioninfo")
+        #print('div', div)
+        anchor = div.find('a')
+
+        image_url = anchor["href"]
+        print(image_url)
+
+
+        #image_url = download_image.get_image_url(image)
+        download_image.download_and_save_image(image_url, f'map_{country}')
+
+        #write a new line into the file
+        file.write(f'<html><img src="map_{country}.png"></html>; {country}\n')
+
+
+print('Done')
+
+#close the file
+file.close()
+
+print('left_out', left_out)
